f715b7ca8b0018573ba677db89909f9fa660a881
Emmanuel Ahenakwah <149573921+pee-21@users.noreply.github.com>
2025-08-04 09:45:08 +0000

    Update 8-script.js

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -1,1 +1,10 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+});
